9274defb38483df217da40a69a0a0fcb4d8c8b4b
Nguyen Huy Long <huylong2113@gmail.com>
2024-03-13 16:21:14 +0700

    Fix url [LongNH]

--- src/utils/appConstants.js
+++ src/utils/appConstants.js
@@ -1,2 +1,2 @@
-export const DEFAULT_API_URL = "https://kohi-neko-one.vercel.app/";
+export const DEFAULT_API_URL = "https://kohi-neko-one.vercel.app";
 // export const DEFAULT_API_URL = "https://kohi-neko.vercel.app";
