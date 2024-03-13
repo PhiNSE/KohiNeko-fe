b2ed8e10edb7428f231330150496fc3dfd0b7a9e
Nguyen Huy Long <huylong2113@gmail.com>
2024-03-13 15:56:03 +0700

    Change url constant [LongNH]

--- src/utils/appConstants.js
+++ src/utils/appConstants.js
@@ -1,2 +1,2 @@
-export const DEFAULT_API_URL = "http://localhost:8000";
+export const DEFAULT_API_URL = "https://kohi-neko-one.vercel.app/";
 // export const DEFAULT_API_URL = "https://kohi-neko.vercel.app";
